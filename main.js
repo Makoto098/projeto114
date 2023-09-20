8c3d02ce9cb7640b455c80e4c490ab116c3eb50f
Makoto098 <144616249+Makoto098@users.noreply.github.com>
2023-09-20 17:21:00 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,92 +1,22 @@
-// Gere um número aleatório como índice para a matriz quickDrawDataset
-const randomNumber = Math.floor(Math.random() * quickDrawDataset.length);
-
-// Imprima o nome do esboço aleatório no console
-console.log(quickDrawDataset[randomNumber]);
-
-// Armazene o nome do esboço aleatório na variável sketch
-const sketch = quickDrawDataset[randomNumber];
-
-// Atualize o texto da tag span com id "drawnSketchText"
-document.getElementById("drawnSketchText").textContent =
-  "Esboço a Ser Desenhado: " + sketch;
-
-// Variáveis adicionais
-let timerCounter = 0;
-let timerCheck = "";
-let drawSketch = "";
-let answerHolder = "";
-let score = 0;
-
-// Função para atualizar a tela
-function updateCanvas() {
-  let canvas = document.getElementById("myCanvas");
-  let ctx = canvas.getContext("2d");
-
-  // Limpar tela
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-  // Definir o fundo da tela como branco
-  ctx.fillStyle = "white";
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
-
-  // Gerar número aleatório
-  let randomNumber = Math.floor(Math.random() * quickDrawDataset.length);
-
-  // Imprimir o valor do esboço no console
-  console.log(randomNumber);
-  sketch = quickDrawDataset[randomNumber];
-
-  // Atualizar o elemento HTML com o esboço
-  document.getElementById("myCanvas").innerText = sketch;
+// Função preload para carregar recursos (pode ser deixada vazia)
+function preload() {
+    // Deixe vazio ou adicione carregamento de recursos aqui
 }
 
-// Função setup
+// Função setup para configurar a tela
 function setup() {
-  let canvas = document.getElementById("myCanvas");
-  canvas.style.display = "block";
-  canvas.style.margin = "0 auto";
-  canvas.style.backgroundColor = "white";
-
-  // Chamar a função draw
-  draw();
-
-  // Verificar se o esboço desenhado é igual ao esboço
-  if (answerHolder === sketch) {
-    answerHolder = "set";
-    score++;
-    document.getElementById("score").innerText = score;
-  }
+    // Crie a tela e coloque-a no centro
+    createCanvas(windowWidth, windowHeight);
+    background(255);
 }
 
-// Função draw
+// Função draw (pode ser deixada vazia)
 function draw() {
-  checkSketch();
+    // Deixe vazio ou adicione desenho personalizado aqui
 }
 
-// Função para verificar o esboço
-function checkSketch() {
-  timerCounter++;
-
-  // Atualizar o tempo
-  document.getElementById("timer").innerText = timerCounter;
-
-  // Imprimir o valor do cronômetro no console
-  console.log(timerCounter);
-
-  // Verificar se o tempo excedeu 400 (ou qualquer outro valor que você desejar)
-  if (timerCounter > 400) {
-    timerCounter = 0;
-    timerCheck = "completed";
-  }
-
-  // Verificar se o cronômetro está completo ou se a resposta já foi definida
-  if (timerCheck === "completed" || answerHolder === "set") {
-    timerCheck = "";
-    answerHolder = "";
-    updateCanvas();
-  }
+// Função para salvar a imagem com filtro
+function salvarImagemComFiltro() {
+    // Lógica para salvar a imagem com filtro aqui
+    // Por exemplo, você pode usar p5.js para aplicar um filtro à imagem e salvar
 }
-
-// Chamar a função de inicialização
-setup();
